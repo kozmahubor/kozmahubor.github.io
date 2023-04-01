11637044e63ef099e498fc47917b3e6090b258a1
kozmahubor <kozmahunor@freemail.hu>
2023-04-01 20:15:26 +0200

    login

--- login.js
+++ login.js
@@ -1,9 +1,10 @@
 function login() {
     const loginId = document.getElementById("loginId").value;
     const loginPw = document.getElementById("loginPw").value;
-    if (loginId == "hubor@admin.com" && loginPw == "admin123") {
+    if (loginId == "asd" && loginPw == "asd") {
         window.location.href = "https://kozmahubor.github.io/mypoems.html";
-        document.getElementById("content").style.display = "block"
+        let doc = document.getElementById("content")
+        doc.style.display = "block"
     }else window.location.href = "https://media.tenor.com/B0FtTWe6TtUAAAAC/peppo-pepe.gif";
     
 }
